8541b2bb773ac5bcdaf0d2324b02ec4e50a4d9c7
KrystianKaminski <krystiankaminski07@gmail.com>
2018-12-08 08:27:14 +0100

    AppBar and header of app created

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,7 +1,37 @@
 import React from 'react'
 
-const Todo = () => (
-    <div>Todo</div>
+import AppBar from 'material-ui/AppBar'
+import Paper from 'material-ui/Paper'
+
+const style = {
+    paper: {
+        margin: 30,
+        padding: 30
+    },
+    header: {
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        color: '#27ae60',
+        fontSize: '20px'
+    }
+}
+
+const Todo = (props) => (
+    <div>
+        <AppBar
+            title="Todo List"
+        />
+        <Paper
+            style={style.paper}
+        >
+            <h1
+                style={style.header}
+            >
+                What have you planned for today?
+            </h1>
+
+        </Paper>
+    </div>
 )
 
 export default Todo
